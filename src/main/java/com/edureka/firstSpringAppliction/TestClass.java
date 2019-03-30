dd559d36e378293b2c7427ceefc0a12df451a0a5
Amandeep Singh (88478) <AMSINGH@amdocs.com>
2019-03-31 05:16:10 +0530

    Firt commit

---
+++ src/main/java/com/edureka/firstSpringAppliction/TestClass.java
@@ -0,0 +1,22 @@
+package com.edureka.firstSpringAppliction;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+
+		ApplicationContext context = new ClassPathXmlApplicationContext("applicationcontext.xml");
+
+		User user =  (User) context.getBean("user");
+		System.out.println(user.getName());
+		
+		Car car= (Car) context.getBean("car");
+		System.out.println(car.getName());
+		System.out.println(car.getCost());		
+		
+		System.out.println(user.getCar().getName());
+	}
+
+}
